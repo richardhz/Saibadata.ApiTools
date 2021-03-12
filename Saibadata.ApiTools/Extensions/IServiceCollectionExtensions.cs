09ae8fdee867c58188e8cea4e081115d001143e6
Richard Hernandez <richardhz@users.noreply.github.com>
2021-03-12 11:27:21 +0000

    Add IServicesCollection Extensions

---
+++ Saibadata.ApiTools/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Saibadata.ApiTools.Interfaces;
+
+namespace Saibadata.ApiTools.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApiTools(this IServiceCollection services)
+        {
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+            services.AddScoped(x =>
+            {
+                var actionContext = x.GetRequiredService<IActionContextAccessor>().ActionContext;
+                var factory = x.GetRequiredService<IUrlHelperFactory>();
+                return factory.GetUrlHelper(actionContext);
+            });
+
+            services.AddScoped<IPageLinksBuilder, PageLinksBuilder>();
+            
+
+            return services;
+        }
+    }
+}
