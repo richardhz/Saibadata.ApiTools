cddd571e21ebb594d1f01b7123e9537e4ace4a37
Richard Hernandez <richardhz@users.noreply.github.com>
2021-03-31 17:32:32 +0100

    ApplySort mappingdictionary is now optional

--- Saibadata.ApiTools/Mapping/IQueryableExtensions.cs
+++ Saibadata.ApiTools/Mapping/IQueryableExtensions.cs
@@ -2,22 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Dynamic.Core;
+using System.Reflection;
 
 namespace Saibadata.ApiTools.Mapping
 {
     public static class IQueryableExtensions
     {
-        public static IQueryable<T> ApplySort<T>(this IQueryable<T> source, string orderBy, Dictionary<string, PropertyMappingValue> mappingDictionary)
+        public static IQueryable<T> ApplySort<T>(this IQueryable<T> source, string orderBy, Dictionary<string, PropertyMappingValue> mappingDictionary = null)
         {
+            PropertyMappingValue propertyMappingValue = null;
+
             if (source is null)
             {
                 throw new ArgumentNullException(nameof(source));
             }
 
-            if (mappingDictionary is null)
-            {
-                throw new ArgumentNullException(nameof(mappingDictionary));
-            }
 
             if (string.IsNullOrWhiteSpace(orderBy))
             {
@@ -41,16 +40,31 @@ namespace Saibadata.ApiTools.Mapping
                 var indexOfFirstSpace = trimmedClause.IndexOf(" ");
                 var propertyName = indexOfFirstSpace == -1 ? trimmedClause : trimmedClause.Remove(indexOfFirstSpace);
 
-                if (!mappingDictionary.ContainsKey(propertyName))
+                if (mappingDictionary is not null)
                 {
-                    throw new ArgumentException($"Key mapping for {propertyName} is missing.");
+                    if (!mappingDictionary.ContainsKey(propertyName))
+                    {
+                        throw new ArgumentException($"Key mapping for {propertyName} is missing.");
+                    }
+
+                    propertyMappingValue = mappingDictionary[propertyName];
+                    if (propertyMappingValue is null)
+                    {
+                        throw new ArgumentNullException("propertyMappingValue");
+                    }
+                }
+                else
+                {
+                    var propertyInfo = typeof(T).GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+                    if (propertyInfo is null)
+                    {
+                        throw new ArgumentException($"Property '{propertyName}' does not exist in class {typeof(T).Name}.");
+                    }
+
+                    propertyMappingValue = new PropertyMappingValue(new List<string> { propertyInfo.Name });
                 }
 
-                var propertyMappingValue = mappingDictionary[propertyName];
-                if (propertyMappingValue is null)
-                {
-                    throw new ArgumentNullException("propertyMappingValue");
-                }
 
                 foreach (var destinationProperty in propertyMappingValue.DestinationProperties)
                 {
